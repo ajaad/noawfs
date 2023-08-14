60106e119f1a0c67dcc2c4341de17db41df85e74
ajaad <anders.johan@konnestad.com>
2023-08-14 13:53:25 +0200

    Now deploy.sh works!

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,16 @@ if false; then
 	# Create a virtual environment
 	python3 -m venv virtEnv
 	source virtEnv/bin/activate
+	
+	# Install a package for building the pywfs-package
+	pip install build
 fi
 
 if true; then
 	# Update package
+	
+	# Activate virtual environment
+	source virtEnv/bin/activate
 	
 	# Uninstall current package
 	pip uninstall pywfs -y
@@ -29,7 +35,7 @@ if true; then
 	rm whl_file/* 
 
 	# Build package from testlab
-	python3 -m build --sdist --outdir ./whl_file  /home/ajaad/Documents/pywfs
+	python3 -m build --outdir ./whl_file --sdist /home/ajaad/Documents/pywfs
 
 	# Install package
 	pip3 install whl_file/pywfs-*
@@ -41,4 +47,7 @@ if false; then
 	# Must be on the same partition to work
 fi
 
-cp /etc/apache2/sites-available/markakartet.lan.conf ./markakartet.lan.conf
+if false; then
+	# Copy/update the apache2 config file:
+	cp /etc/apache2/sites-available/markakartet.lan.conf ./markakartet.lan.conf
+fi
